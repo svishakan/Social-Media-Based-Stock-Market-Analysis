ac20ef46ff762994f0db581354b164fbb249bbc3
Venkataraman-Nagarajan <venkataraman@motorq.com>
2022-04-20 13:05:20 +0530

    Minor bug fix: tweet fetcher

--- Database/tweetLoader.py
+++ Database/tweetLoader.py
@@ -1,39 +1,49 @@
 # Import required modules
 import csv
 import sqlite3
+import os
 
 # Connecting to the FYP database
-connection = sqlite3.connect('fypdb.db')
+connection = sqlite3.connect(os.path.join(os.path.dirname(__file__), f'fypdb.db'))
 
 # Creating a cursor object to execute SQL queries on a database table
 cursor = connection.cursor()
 
+
+FILENAME = "NewTweetData.csv"
+TABLENAME = "tweets_count"
+
 # Table Definition
-create_table = '''CREATE TABLE tweets(
+create_table = f'''CREATE TABLE {TABLENAME}(
 				category TEXT,
 				tweetDate DATE,
-				tweet TEXT NOT NULL);
+				count INTEGER,
+				tweet TEXT NOT NULL,
+				CONSTRAINT uniq_twt PRIMARY KEY (category, tweetDate, tweet)
+       			);
 				'''
-
 # Creating the table into our
 # database
-# cursor.execute(create_table)
+cursor.execute(create_table)
 
 # Opening the NewTweetData.csv file
-file = open('../Datasets/NewTweetData.csv')
-
+file = open(os.path.join(os.path.dirname(__file__),f"../Datasets/{FILENAME}"))
 # Reading the contents of the NewTweetData.csv file
 contents = csv.reader(file, delimiter='\t')
 
 # SQL query to insert data into the tweets table
-insert_records = "INSERT INTO tweets (category, tweetDate, tweet) VALUES(?, ?, ?)"
+try:
+	insert_records = f"INSERT INTO {TABLENAME} (category, tweetDate, count, tweet) VALUES(?, ?, ?, ?) ON CONFLICT(category, tweetDate, tweet) DO NOTHING"
+ 	# Importing the contents of the file into our tweets table
+	cursor.executemany(insert_records, contents)
+except sqlite3.Error as error:
+    print({error})
+    pass
 
-# Importing the contents of the file into our tweets table
-cursor.executemany(insert_records, contents)
 
 # SQL query to retrieve all data from the tweets table to verify that the
 # data of the csv file has been successfully inserted into the table
-select_all = "SELECT * FROM tweets"
+select_all = f"SELECT * FROM {TABLENAME}"
 rows = cursor.execute(select_all).fetchall()
 
 # Output to the console screen
