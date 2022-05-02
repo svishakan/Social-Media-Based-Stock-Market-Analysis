8da835dd1ab864b64aacf1920dbb46a90e40e312
Vishakan Subramanian <svishakan123@yahoo.com>
2022-05-02 23:05:42 +0530

    Performed some code cleaning.

--- Scripts/NewsDataFetcher.py
+++ Scripts/NewsDataFetcher.py
@@ -7,23 +7,25 @@ import NewsAPICallScripts.NewsData as NewsData
 import NewsAPICallScripts.NewsCatcherData as NewsCatcherData
 import NewsAPICallScripts.NYTData as NYTData
 
-import sys, getopt
+import sys
+import getopt
+
 
 if __name__ == "__main__":
     filename = "NewsData.csv"
-    
+
     argList = sys.argv[1:]
-    
+
     options = "gf:n:o"
     long_options = ["global", "file =", "new_file =", "old_file"]
-    
+
     try:
         arguments, values = getopt.getopt(argList, options, long_options)
-        
+
         if(len(argList) == 0):
-            NewsData.main(filename);
-            NewsCatcherData.main(filename);
-            NYTData.main(filename);
+            NewsData.main(filename)
+            NewsCatcherData.main(filename)
+            NYTData.main(filename)
         else:
             for curArg, curVal in arguments:
                 if(curArg == "-g" or curArg == "--global"):
@@ -34,13 +36,12 @@ if __name__ == "__main__":
                     filename = curVal
                 elif(curArg == "-n" or curArg == "--new_file"):
                     filename = curVal
-                
+
                 # print(curArg, curVal)
-                NewsData.main(filename);
-                NewsCatcherData.main(filename);
-                NYTData.main(filename);
+                NewsData.main(filename)
+                NewsCatcherData.main(filename)
+                NYTData.main(filename)
             pass
     except getopt.error as err:
-        # output error, and return with an error code
-        print (str(err))
-    
+        # log the error and exit
+        print(str(err))
